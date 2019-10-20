ac8999c25efa68140db40684e80443039d872085
Mao Li <anrenyil@gmail.com>
2019-10-19 22:01:00 -0700

    browser notification

--- app/javascript/channels/chatrooms_channel.js
+++ app/javascript/channels/chatrooms_channel.js
@@ -13,6 +13,21 @@ consumer.subscriptions.create("ChatroomsChannel", {
     if (activeChatroom) {
       activeChatroom.appendChild(document.createRange().createContextualFragment(data.message));
     } else {
+
+    }
+    if (document.visibilityState == "hidden") {
+      const img_url = $(data.message).children("img")[0].src;
+      const childDiv = $(data.message).children("div");
+      let a = new Notification(`${childDiv.children("strong")[0].innerText}: `, {
+        body: childDiv.children("p")[0].innerText,
+        icon: img_url,
+        renotify: true,
+        tag: childDiv.children("p")[0].innerText,
+        silent: true
+      });
+      setTimeout(function () {
+        a.close();
+      }, 5000);
       // document.querySelector(`[data-behavior='chatroom-link'][data-chatroom-id='${data.chatroom_id}']`)
     }
 
